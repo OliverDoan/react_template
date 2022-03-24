a992ff4b462836570c09edad526b7851f50bd6b3
OliverDoan <doanvietthuan1005@gmail.com>
2022-03-24 09:07:25 +0700

    Bài 11 - Conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,15 +6,17 @@ import TodoItem from "./components/learn_code_tokyo/todoItem";
 class App extends React.Component {
   constructor() {
     super();
-    this.todoItems = ["React", "Hook", "Redux"];
+    this.todoItems = [];
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          {this.todoItems.map((item, index) => (
-            <TodoItem key={index} title={item} />
-          ))}
+          {this.todoItems.length > 0 &&
+            this.todoItems.map((item, index) => (
+              <TodoItem key={index} title={item} />
+            ))}
+          {this.todoItems.length === 0 && "Nothing"}
         </header>
       </div>
     );
