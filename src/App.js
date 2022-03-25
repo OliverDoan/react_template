317214793deffef69ba459f64e493e28a2fd6a33
OliverDoan <doanvietthuan1005@gmail.com>
2022-03-25 15:55:17 +0700

    change app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import React from "react";
 
-import "./App.css";
-import TodoItem from "./components/learn_code_tokyo/todoItem";
-
-class App extends React.Component {
-  constructor() {
-    super();
-    this.todoItems = [];
-  }
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          {this.todoItems.length > 0 &&
-            this.todoItems.map((item, index) => (
-              <TodoItem key={index} title={item} />
-            ))}
-          {this.todoItems.length === 0 && "Nothing"}
-        </header>
-      </div>
-    );
-  }
+function App(props) {
+  return <div>hello</div>;
 }
 
 export default App;
