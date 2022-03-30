71d4c2c816e0fa0e04a3c794416ff91c962d0dde
OliverDoan <doanvietthuan1005@gmail.com>
2022-03-30 15:47:20 +0700

    React Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import App from "./components/App";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
+class Root extends Component {
+  render() {
+    return (
+      <>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Root />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
