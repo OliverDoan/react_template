14bb40acb4b2461f110d63dad34d82a62e58e747
OliverDoan <doanvietthuan1005@gmail.com>
2022-03-30 16:17:14 +0700

    frontend login and register page

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,9 +1,50 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
+} from "semantic-ui-react";
 
-export class Login extends Component {
-  render() {
-    return <div>Login</div>;
-  }
-}
+const Login = () => (
+  <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as="h2" color="teal" textAlign="center">
+        Log-in to your account
+      </Header>
+      <Form size="large">
+        <Segment stacked>
+          <Form.Input
+            fluid
+            icon="mail"
+            iconPosition="left"
+            placeholder="E-mail address"
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Password"
+            type="password"
+          />
+
+          <Button color="teal" fluid size="large">
+            Login
+          </Button>
+        </Segment>
+      </Form>
+      <Message error>
+        <h3>Error</h3>
+        Error message
+      </Message>
+      <Message>
+        Don't have an account? <Link to="/register"> Sign Up</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
+);
 
 export default Login;
--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -1,9 +1,63 @@
-import React, { Component } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment,
+} from "semantic-ui-react";
 
-export class Register extends Component {
-  render() {
-    return <div>Register</div>;
-  }
-}
+const Register = () => (
+  <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+    <Grid.Column style={{ maxWidth: 450 }}>
+      <Header as="h2" color="teal" textAlign="center">
+        Register for WorkList
+      </Header>
+      <Form size="large">
+        <Segment stacked>
+          <Form.Input
+            fluid
+            icon="user"
+            iconPosition="left"
+            placeholder="Username"
+          />
+          <Form.Input
+            fluid
+            icon="mail"
+            iconPosition="left"
+            placeholder="E-mail address"
+          />
+          <Form.Input
+            fluid
+            icon="lock"
+            iconPosition="left"
+            placeholder="Password"
+            type="password"
+          />
+          <Form.Input
+            fluid
+            icon="repeat"
+            iconPosition="left"
+            placeholder="Password Confirmation"
+            type="password"
+          />
+
+          <Button color="teal" fluid size="large">
+            Submit
+          </Button>
+        </Segment>
+      </Form>
+      <Message error>
+        <h3>Error</h3>
+        Error message
+      </Message>
+      <Message>
+        Already a user <Link to="/login"> Sign in</Link>
+      </Message>
+    </Grid.Column>
+  </Grid>
+);
 
 export default Register;
